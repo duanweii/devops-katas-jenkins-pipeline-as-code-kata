60950139df58d011aad1fb5687a2f4a67d946e38
duanwei <duanwei@duanweideMacBook-Pro.local>
2020-11-25 20:18:36 +0800

    AdminService with a test

---
+++ src/main/java/devops/katas/adminprovider/AdminproviderApplication.java
@@ -0,0 +1,63 @@
+package devops.katas.adminprovider;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class AdminproviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminproviderApplication.class, args);
+    }
+
+}
+
+@Configuration
+class AdminConfiguration {
+    @Bean
+    AdminService adminService() {
+        return new AdminService();
+    }
+}
+
+class AdminService {
+    public Admin retrieveAdmin(int id) {
+        return new Admin("firstName [" + id + "]", "lastName [" + id + "]");
+    }
+}
+
+
+@RestController
+class AdminController {
+    @Autowired
+    AdminService adminService;
+
+    @GetMapping("/admin/{id}")
+    Admin admin(@PathVariable int id) {
+        return adminService.retrieveAdmin(id);
+    }
+}
+
+class Admin {
+    private final String firstName;
+    private final String lastName;
+
+    public Admin(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
---
+++ src/test/java/devops/katas/adminprovider/AdminproviderApplicationTests.java
@@ -0,0 +1,20 @@
+package devops.katas.adminprovider;
+
+import org.assertj.core.api.BDDAssertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminproviderApplicationTests {
+
+	@Test
+	public void should_retrieve_an_admin_with_correct_names() {
+		AdminService adminService = new AdminService();
+
+		Admin admin = adminService.retrieveAdmin(4);
+
+		BDDAssertions.then(admin.getFirstName()).isEqualTo("firstName [4]");
+		BDDAssertions.then(admin.getLastName()).isEqualTo("lastName [4]");
+	}
+
+}
