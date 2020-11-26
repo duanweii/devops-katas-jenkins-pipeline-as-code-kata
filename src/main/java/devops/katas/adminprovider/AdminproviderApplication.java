c49e86f0689e5d0ca585176965c6af55bfc17206
duanwei <duanwei@duanweideMacBook-Pro.local>
2020-11-26 19:59:36 +0800

    update jdk version

--- src/main/java/devops/katas/adminprovider/AdminproviderApplication.java
+++ src/main/java/devops/katas/adminprovider/AdminproviderApplication.java
@@ -15,7 +15,6 @@ public class AdminproviderApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdminproviderApplication.class, args);
     }
-
 }
 
 @Configuration
